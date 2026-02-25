9be5106a6a5de96195d6c42a026f623836765e95
Gyan163 <gyanbijlani@gmail.com>
2026-02-25 15:42:56 +0530

    Add UC4: Palindrome checker using char array two-pointer approach

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,28 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String word = "level";
-        String reversed = "";
+        String word = "radar";
+        char[] chars = word.toCharArray();
+        boolean isPalindrome = true;
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
         System.out.println("=======================================");
-        System.out.println("   Palindrome Checker App - UC3");
+        System.out.println("   Palindrome Checker App - UC4");
         System.out.println("   Version: 1.0");
         System.out.println("=======================================");
         System.out.println("Checking word: " + word);
 
-        if (word.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("Result: \"" + word + "\" is a palindrome.");
         } else {
             System.out.println("Result: \"" + word + "\" is not a palindrome.");
